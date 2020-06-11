5b5abac98a793b25b1d9ccb872aaec5a7ac14ba3
wangliu1102 <wangliu.ah@qq.com>
2020-06-11 10:20:31 +0800

    添加课程信息

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/mapper/EduChapterMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.eduservice.mapper;
+
+import com.atguigu.eduservice.entity.EduChapter;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 课程章节 Mapper 接口
+ * </p>
+ *
+ * @author 王柳
+ * @since 2020-06-11
+ */
+public interface EduChapterMapper extends BaseMapper<EduChapter> {
+
+}
---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/EduChapterService.java
@@ -0,0 +1,16 @@
+package com.atguigu.eduservice.service;
+
+import com.atguigu.eduservice.entity.EduChapter;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程章节 服务类
+ * </p>
+ *
+ * @author 王柳
+ * @since 2020-06-11
+ */
+public interface EduChapterService extends IService<EduChapter> {
+
+}
