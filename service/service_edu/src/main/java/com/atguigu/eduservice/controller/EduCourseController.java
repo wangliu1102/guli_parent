e3a651d32dfe5949243a469ec7ef467b32ef3969
wangliu1102 <wangliu.ah@qq.com>
2020-06-11 13:52:07 +0800

    修改课程信息

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduCourseController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduCourseController.java
@@ -30,5 +30,19 @@ public class EduCourseController {
         String id = courseService.saveCourseInfo(courseInfoVo);
         return R.ok().data("courseId", id);
     }
+
+    //根据课程id查询课程基本信息
+    @GetMapping("getCourseInfo/{courseId}")
+    public R getCourseInfo(@PathVariable String courseId) {
+        CourseInfoVo courseInfoVo = courseService.getCourseInfo(courseId);
+        return R.ok().data("courseInfoVo",courseInfoVo);
+    }
+
+    //修改课程信息
+    @PostMapping("updateCourseInfo")
+    public R updateCourseInfo(@RequestBody CourseInfoVo courseInfoVo) {
+        courseService.updateCourseInfo(courseInfoVo);
+        return R.ok();
+    }
 }
 
