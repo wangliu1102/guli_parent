f95ed1d1400e2c12b4e99f7b8f6ae0d39aede725
wangliu1102 <wangliu.ah@qq.com>
2020-06-12 16:01:54 +0800

    阿里云视频点播-视频删除

--- service/service_vod/src/main/java/com/atguigu/vod/service/impl/VodServiceImpl.java
+++ service/service_vod/src/main/java/com/atguigu/vod/service/impl/VodServiceImpl.java
@@ -3,12 +3,18 @@ package com.atguigu.vod.service.impl;
 import com.aliyun.vod.upload.impl.UploadVideoImpl;
 import com.aliyun.vod.upload.req.UploadStreamRequest;
 import com.aliyun.vod.upload.resp.UploadStreamResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.vod.model.v20170321.DeleteVideoRequest;
+import com.atguigu.servicebase.exceptionhandler.GuliException;
 import com.atguigu.vod.Utils.ConstantVodUtils;
+import com.atguigu.vod.Utils.InitVodCilent;
 import com.atguigu.vod.service.VodService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
+import java.util.List;
 
 @Service
 public class VodServiceImpl implements VodService {
@@ -42,10 +48,32 @@ public class VodServiceImpl implements VodService {
                 System.out.print("ErrorMessage=" + response.getMessage() + "\n");
             }
             return videoId;
-        }catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
 
     }
+
+    @Override
+    public void removeMoreAlyVideo(List videoIdList) {
+        try {
+            //初始化对象
+            DefaultAcsClient client = InitVodCilent.initVodClient(ConstantVodUtils.ACCESS_KEY_ID, ConstantVodUtils.ACCESS_KEY_SECRET);
+            //创建删除视频request对象
+            DeleteVideoRequest request = new DeleteVideoRequest();
+
+            //videoIdList值转换成 1,2,3
+            String videoIds = StringUtils.join(videoIdList.toArray(), ",");
+
+            //向request设置视频id
+            request.setVideoIds(videoIds);
+            //调用初始化对象的方法实现删除
+            client.getAcsResponse(request);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new GuliException(20001, "删除视频失败");
+        }
+    }
+
 }
